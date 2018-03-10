881987916639fdd1a3130b112844e3ccd7c4574a
Taras Kustov <tkusto@gmail.com>
2018-03-10 15:19:03 +0200

    video-feed-item refactoring: use ng-switch instead of templates

--- src/video-feed/index.js
+++ src/video-feed/index.js
@@ -1,16 +1,13 @@
 import angular from "angular";
+import tkYouTube from "../youtube";
 import videoFeed from "./video-feed";
-import {
-    default as videoFeedItem,
-    registerTemplates as regVideoFeedItemTemplates
-} from "./video-feed-item";
+import videoFeedItem from "./video-feed-item";
 import videoViewsCount from './video-views-count';
 
 const MODULE_NAME = "tk.VideoFeed";
 export default MODULE_NAME;
 
-angular.module(MODULE_NAME, []).
-    run(regVideoFeedItemTemplates).
+angular.module(MODULE_NAME, [tkYouTube]).
     filter("tkVideoViewsCount", videoViewsCount).
     component("tkVideoFeed", videoFeed).
     component("tkVideoFeedItem", videoFeedItem);
--- src/video-feed/video-feed-item.js
+++ src/video-feed/video-feed-item.js
@@ -3,13 +3,11 @@ const reUrlTitle = /\/([^/]+?)(?:\.[^/]+)?$/;
 const SOURCES = {
     "youtube": {
         getUrl: ({ videoId }) => `https://www.youtube.com/embed/${videoId}`,
-        getTitle: ({ title }) => title,
-        template: "tk-video-feed-item/youtube"
+        getTitle: ({ title }) => title
     },
     "facebook": {
         getUrl: ({ videoId }) => `https://www.facebook.com/video.php?v=${videoId}`,
         getTitle: ({ title }) => title,
-        template: "tk-video-feed-item/facebook",
         init: () => setTimeout(() => {
             try {
                 FB.XFBML.parse();
@@ -23,8 +21,7 @@ const SOURCES = {
         getTitle: ({ url }) => {
             const match = url.match(reUrlTitle);
             return match ? match[1] : "Video";
-        },
-        template: "tk-video-feed-item/url"
+        }
     }
 };
 
@@ -32,7 +29,6 @@ class VideoFeedItem {
     static get $inject() { return ["$sce"]; }
     constructor($sce) {
         this.$sce = $sce;
-        this.template = undefined;
     }
 
     $onChanges({ item }) {
@@ -40,7 +36,6 @@ class VideoFeedItem {
             const { source } = item.currentValue;
             if (source && SOURCES.hasOwnProperty(source)) {
                 const { getUrl, getTitle, template, init } = SOURCES[source];
-                this.template = template;
                 this.videoUrl = this.$sce.trustAsResourceUrl(getUrl(item.currentValue));
                 this.videoTitle = getTitle(item.currentValue);
                 if (typeof init === "function") {
@@ -51,27 +46,6 @@ class VideoFeedItem {
     }    
 }
 
-registerTemplates.$inject = ["$templateCache"];
-export function registerTemplates($templatesCache) {
-    $templatesCache.put("tk-video-feed-item/youtube", `
-        <iframe ng-src="{{$videoFeedItem.videoUrl}}"
-                width="420"
-                height="200"></iframe>
-    `);
-    $templatesCache.put("tk-video-feed-item/facebook", `
-        <div class="fb-video"
-             ng-attr-data-href="{{$videoFeedItem.videoUrl}}"
-             data-width="420"
-             data-allowfullscreen="true"></div>
-    `);
-    $templatesCache.put("tk-video-feed-item/url", `
-        <video ng-src="{{$videoFeedItem.videoUrl}}"
-               width="420"
-               heihgt="315"
-               controls></video>
-    `);
-}
-
 export default {
     bindings: {
         item: "<"
@@ -79,12 +53,29 @@ export default {
     controller: VideoFeedItem,
     controllerAs: "$videoFeedItem",
     template: `
-        <section class="item">
+        <section class="item" ng-switch="$videoFeedItem.item.source">
             <header class="item-header">
                 <span class="item-title" ng-bind="$videoFeedItem.videoTitle"></span>
                 <span class="item-views" ng-bind="$videoFeedItem.item.views | tkVideoViewsCount"></span>
             </header>
-            <div class="item-video" ng-if="$videoFeedItem.template" ng-include="$videoFeedItem.template"></div>
+            <!-- YOUTUBE -->
+            <div class="item-video" ng-switch-when="youtube">
+                <tk-yt-player video-id="{{$videoFeedItem.item.videoId}}" width="480"/>
+            </div>
+            <!-- FACEBOOK -->
+            <div class="item-video" ng-switch-when="facebook">
+                <div class="fb-video"
+                        ng-attr-data-href="{{$videoFeedItem.videoUrl}}"
+                        data-width="480"
+                        data-allowfullscreen="true"></div>
+            </div>
+            <!-- VIDEO URL -->
+            <div class="item-video" ng-switch-when="url">
+                <video ng-src="{{$videoFeedItem.videoUrl}}"
+                        width="480"
+                        heihgt="270"
+                        controls></video>
+            </div>
         </section>
     `
 }
